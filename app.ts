209dfd3481c119adfa6812c8a908b29a5afbcbd8
bartvanhoey <bartvanhoey@hotmail.com>
2018-10-11 18:52:54 +0200

    end clip Demo: Using the Spread Operator

--- app.ts
+++ app.ts
@@ -4,11 +4,11 @@ import { UniversityLibrarian, ReferenceItem} from './classes';
 import * as util from './lib/utilityFunctions';
 
 function PrintBookInfo(book: Book): void {
-    console.log(`${book.title} was authored by ${book.author}`);
+   // console.log(`${book.title} was authored by ${book.author}`);
 }
 
 function PrintBookInfoWithDestruct({title: bookTitle, author: bookAuthor}: Book): void {
-    console.log(`${bookTitle} was authored by ${bookAuthor}`);
+    //console.log(`${bookTitle} was authored by ${bookAuthor}`);
 }
 
 let [book1, book2] = util.GetAllBooks();
@@ -25,18 +25,35 @@ function LogFavoriteBooks([book1, book2, ...others]: Book[]){
 LogFavoriteBooks(util.GetAllBooks());
 
 let { title, author } = book1;
-console.log('\n')
-console.log('Details Book1')
-console.log("title book1: ", title)
-console.log("author book1: ", author)
+// console.log('\n')
+// console.log('Details Book1')
+// console.log("title book1: ", title)
+// console.log("author book1: ", author)
 
 let { title: bookTitle, author: bookAuthor } = book2;
-console.log('\n')
-console.log('Details Book2')
-console.log("title book2: ", bookTitle)
-console.log("author book2: ", bookAuthor)
-console.log('\n')
+// console.log('\n')
+// console.log('Details Book2')
+// console.log("title book2: ", bookTitle)
+// console.log("author book2: ", bookAuthor)
+// console.log('\n')
 
 
-console.log("Print Book details with destruct")
+//console.log("Print Book details with destruct")
 PrintBookInfoWithDestruct(book1);
+
+let schoolBooks: Book[] = [
+    { id: 10, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', available: true, category: Category.Fiction },
+    { id: 11, title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', available: true, category: Category.Fiction },
+    { id: 12, title: 'Clear Light of Day', author: 'Anita Desai', available: true, category: Category.Fiction }
+];
+
+let booksRead: Book[] = util.GetAllBooks();
+booksRead.push(...schoolBooks);
+//console.log(booksRead);
+
+
+let poets: string[] = ['Shelley', 'Collings', 'Hughes'];
+let authors: string[] = ['Tolstoy', 'Fitzgerald', ...poets];
+console.log(authors)
+
+
