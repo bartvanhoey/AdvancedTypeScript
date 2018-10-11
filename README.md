a2edddfb26ab0e2caf3501ada1f5b4a390e234e9
bartvanhoey <bartvanhoey@hotmail.com>
2018-10-11 16:42:48 +0200

    commit after gitignore added

---
+++ README.md
@@ -0,0 +1,17 @@
+# Advanced Typescript
+
+## Desctructuring arrays
+
+`let colors: string[] = ['green', ' yellow', 'blue'];`
+`let [firstColor, secondColor, thirdColor] = colors;`
+
+## Desctructuring objects
+
+`let person = {
+    name: 'Bart',
+    address: '123 High Street'
+    phone: '333-444-555'};`
+`let { name, address phone } = person;`
+
+
+
---
+++ classes.ts
@@ -0,0 +1,59 @@
+import * as Interfaces from './interfaces';
+
+
+class Employee {
+    title: string;
+
+    addToSchedule(): void {
+        console.log('Employee added to schedule.');
+    }
+
+    logTitle(): void {;
+        console.log(`Employee has the title ${this.title}.`);
+    }
+}
+
+class Researcher {
+
+    doResearch(topic: string): void {
+        console.log(`Doing research on ${topic}.`);
+    }
+}
+
+class UniversityLibrarian implements Interfaces.Librarian {
+    
+    name: string;
+    email: string;
+    department: string;
+    
+    assistCustomer(custName: string) {
+        console.log(this.name + ' is assisting ' + custName);
+    }
+}
+
+abstract class ReferenceItem {
+    
+    private _publisher: string;
+    static department: string = 'Research';
+
+    constructor(public title: string, protected year: number) {
+        console.log('Creating a new ReferenceItem...');
+    }
+    
+    printItem(): void {
+        console.log(`${this.title} was published in ${this.year}.`);
+        console.log(`Department: ${ReferenceItem.department}`);
+    }
+    
+    get publisher(): string {
+        return this._publisher.toUpperCase();
+    }
+    
+    set publisher(newPublisher: string) {
+        this._publisher = newPublisher;
+    }
+    
+    abstract printCitation(): void;
+}
+
+export { UniversityLibrarian, ReferenceItem, Employee, Researcher };
